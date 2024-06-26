ada7d83cc6f0981c5949ad1129944f7c4af92b0d
Severin Lochschmidt <seviron53@gmail.com>
2024-06-26 13:04:27 +0200

    Update to 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix Flake for 5G system emulation frameworks";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs, ... }:
